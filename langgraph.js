b3e89b4e580c01e53fbf43b4b0c0f452e9353ae1
SUJITP <sujitpatel1111@gmail.com>
2025-11-07 15:10:35 +0530

    updated  code

--- langgraph.js
+++ langgraph.js
@@ -1,3 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
+
+import readline from "readline/promises";
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+async function main(){
+    const userInput = await rl.question("You: ")
+    console.log("You Said : ", userInput )
+}
+
+
+main()

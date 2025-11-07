0ca499adcd5e99d669d19bcf9d51dd89ddba029b
SUJITP <sujitpatel1111@gmail.com>
2025-11-07 17:29:19 +0530

    updated  code

--- langgraph.js
+++ langgraph.js
@@ -2,17 +2,18 @@ import { END, MessagesAnnotation, START, StateGraph } from "@langchain/langgraph
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 import { ChatGroq } from "@langchain/groq";
-
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { DynamicStructuredTool } from "@langchain/core/tools";
+import { z } from "zod";
+import { tavily } from "@tavily/core";
 
 
 
 import readline from "readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
-const llm = new ChatGroq({
-    apiKey: process.env.GROQ_API_KEY,
-    model: "llama-3.3-70b-versatile"
-})
+
+
 
 async function callModel(state) {
     console.log("🧠 Calling Groq LLM...");
@@ -21,13 +22,54 @@ async function callModel(state) {
 }
 
 
+const tavilyTool = new DynamicStructuredTool({
+  name: "tavily_search",
+  description: "Search the web using Tavily for real-time information.",
+  schema: z.object({
+    query: z.string().describe("The query to search on the web."),
+  }),
+  func: async ({ query }) => {
+    const tvly = tavily({ apiKey: process.env.TRAVILEY_API_KEY });
+    const result = await tvly.search(query);
+    return JSON.stringify(result.results.slice(0, 2));
+  },
+});
+
+
+const tools = [tavilyTool];
+const toolNode = new ToolNode(tools);
+
+const llm = new ChatGroq({
+    apiKey: process.env.GROQ_API_KEY,
+    model: "llama-3.3-70b-versatile"
+}).bindTools(tools)
+
+
+
 // We need to Provide the feature of Browsing Internet to the our LLM. 
 // This will make an Agents
 
+
+
+function shouldContinue(state) {
+  const lastMessage = state.messages[state.messages.length - 1];
+  if (lastMessage.tool_calls && lastMessage.tool_calls.length > 0) {
+    return "tools";
+  }
+  return "__end__";
+}
+
+
+// Langgraph Workflow
 const workflow = new StateGraph(MessagesAnnotation);
 workflow.addNode("agent", callModel);
+workflow.addNode("tools", toolNode);
 workflow.addEdge(START, "agent");
-workflow.addEdge("agent", END);
+workflow.addConditionalEdges("agent", shouldContinue, {
+  tools: "tools",
+  __end__: END,
+});
+workflow.addEdge("tools", "agent");
 const app = workflow.compile();
 
 
@@ -45,11 +87,11 @@ async function main() {
             messages: [{ role: "user", content: userInput }],
         });
 
-        console.log("AI Message", finalState.messages[1].content)
+        console.log("AI Message", finalState)
 
     }
 
     rl.close();
 }
 
-main();
+main()
